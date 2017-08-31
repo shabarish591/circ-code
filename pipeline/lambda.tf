e0b2c20306c85420309e69cca83a2aec53c9604f
14mar1983 <14mar1983@gmail.com>
2017-08-31 21:39:10 +0530

    Update lambda.tf

--- pipeline/lambda.tf
+++ pipeline/lambda.tf
@@ -3,11 +3,11 @@ provider "aws" {
 }
 
 resource "aws_lambda_function" "ngp_automation_switchboard" {
-  filename         = "ngp_automation_switchboard.zip"
+  filename         = "~/ngp_automation_switchboard.zip"
   function_name    = "ngp_automation_switchboard"
   role             = "arn:aws:iam::309048944729:role/lambda_exec_role"
   handler          = "switchboard.handler"
-  source_code_hash = "${base64sha256(file("ngp_automation_switchboard.zip"))}"
+  source_code_hash = "${base64sha256(file("~/ngp_automation_switchboard.zip"))}"
   runtime          = "python2.7",
 	timeout	    = 60,
 	vpc_config {
